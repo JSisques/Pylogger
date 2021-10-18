550899f34da0f2632140ed1b91cc14519b385fce
JSisques <javierplazasisques@gmail.com>
2021-10-19 00:23:09 +0200

    Root commit

---
+++ directory_manager.py
@@ -0,0 +1,15 @@
+class DirectoryManager:
+
+    def __init__(self) -> None:
+        pass
+
+    def check_if_exists(self, path) -> bool:
+        pass
+
+    def create_directory(self, path) -> str:
+        #devuelve la ruta absoulta creada
+        pass
+
+    def remove_directory(self, path) -> str:
+        #devuelve la ruta absoluta borrada
+        pass
---
+++ keylogger.py
@@ -0,0 +1,14 @@
+
+class Keylogger:
+    
+    def __init__(self) -> None:
+        pass
+
+    def get_key_pressed(self):
+        pass
+
+    def write_log(self):
+        pass
+
+    def start(self):
+        pass
